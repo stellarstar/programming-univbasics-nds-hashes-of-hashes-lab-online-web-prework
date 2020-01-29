18f7057aed5aa124713b04206bbe1b59236cd5fe
victor tim <technicalpro@securemedia.ch>
2020-01-29 02:02:48 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -1,45 +1,21 @@
-# Build a nested HoH of the classification of biological life on Earth based
-# on these commented-out snippets assigned to PORTION_1-PORTION_4. Return
-# that HoH as the return value for the method "naming_system."
-#
-# The tests will guide your construction.
-#
-# PORTION_1 = {
-#   label: "Kingdom",
-#   sub_category: {
-#     label: "Phylum",
-#     sub_category: {
-#       label: "Class"
-#     }
-#   }
-# }
-# 
-# PORTION_2 = {
-#   label: "Order"
-# }
-# 
-# PORTION_3 = {
-#   label: "Family",
-#   sub_category: {
-#     label: "Genus",
-#   }
-# }
-# 
-# PORTION_4 = {
-#   label: "Species",
-#   sub_category: nil
-# }
-
 def naming_system
-  # Remember:
-  #  Kingdom
-  #  Phylum
-  #  Class
-  #  Order
-  #  Family
-  #  Genus
-  #  Species
-  # So, if we have the "Kingdom" node we should be able to "tunnel" into the
-  # HoH all the way to Species!
-
+  child = {
+  :name=>["Carrie Fisher"],
+  :mother=>[]
+  },
+  child2 = {
+  :name=>["Debbie Reynolds"],
+  :mother=>[],
+  :name=> ["Maxene Reynolds"]
+  },
+  child3 = {
+  :father=>[]
+  },
+  child4 = {
+  :father=>[],
+  :name=>["Eddie Fisher"],
+  :mother=>[],
+  :father=>[]
+  },
+}
 end
