4249d81e628cbbf649ace985ab1c4b6399d91255
victor tim <technicalpro@securemedia.ch>
2020-01-29 22:18:49 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -11,10 +11,21 @@ def naming_system
 
   label : "Order",
   sub_category :{
+    
+  label: "Family",
+  sub_category: {
+  
+  label: "Genus",
+  sub_category: {
+    
+  label: "Species",
+  sub_category: nil: {
  }
   }
     }
       }
         }
-
+          }
+            }
+              }
 end
